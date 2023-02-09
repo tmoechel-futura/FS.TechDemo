0a93b033fda64213daecfb5e1c64533b2ac656c4
tmoechel-futura <t.moechel@futura-solutions.de>
2023-02-09 10:48:06 +0100

    add most used commands

--- Deploy1.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location -Path "C:\Entwicklung\Learning\FS.TechDemo"
-kubectl apply -f FS.TechDemo.BuyerBFF/buyer-bff.yml
-kubectl apply -f FS.TechDemo.OrderService/order-service.yml
-
---
+++ deployment/scripts/deploy.ps1
@@ -0,0 +1,9 @@
+﻿Set-Location -Path "C:\Entwicklung\Learning\FS.TechDemo"
+docker build . -t futuratechdemoacr.azurecr.io/order-service:1.2 -f FS.TechDemo.OrderService/Dockerfile
+docker build . -t futuratechdemoacr.azurecr.io/delivery-service:1.2 -f FS.TechDemo.DeliveryService/Dockerfile
+docker build . -t futuratechdemoacr.azurecr.io/buyer-bff:1.2 -f FS.TechDemo.BuyerBFF/Dockerfile
+
+docker push futuratechdemoacr.azurecr.io/order-service:1.2
+docker push futuratechdemoacr.azurecr.io/delivery-service:1.2
+docker push futuratechdemoacr.azurecr.io/buyer-bff:1.2
+
